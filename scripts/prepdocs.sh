ba0bfe083ef430481216d5424147e8dbb7357751
Samuel Lee <129730229+samuellee77@users.noreply.github.com>
2023-08-31 16:38:21 +0800

    efit

--- scripts/prepdocs.sh
+++ scripts/prepdocs.sh
@@ -11,11 +11,5 @@ done <<EOF
 $(azd env get-values)
 EOF
 
-echo 'Creating python virtual environment "scripts/.venv"'
-python3 -m venv scripts/.venv
-
-echo 'Installing dependencies from "requirements.txt" into virtual environment'
-./scripts/.venv/bin/python -m pip install -r scripts/requirements.txt
-
 echo 'Running "prepdocs.py"'
-./scripts/.venv/bin/python ./scripts/prepdocs.py './data/*.md' --storageaccount "$AZURE_STORAGE_ACCOUNT" --container "$AZURE_STORAGE_CONTAINER" --storagekey "$AZURE_STORAGE_KEY" --searchservice "$AZURE_SEARCH_SERVICE" --openaiservice "$AZURE_OPENAI_SERVICE" --index "$AZURE_SEARCH_INDEX" --tenantid "$AZURE_TENANT_ID" -v
+python ./scripts/prepdocs.py './data/*.md' --storageaccount "$AZURE_STORAGE_ACCOUNT" --container "$AZURE_STORAGE_CONTAINER" --storagekey "$AZURE_STORAGE_KEY" --searchservice "$AZURE_SEARCH_SERVICE" --openaiservice "$AZURE_OPENAI_SERVICE" --index "$AZURE_SEARCH_INDEX" --tenantid "$AZURE_TENANT_ID" --remove_image --remove_href -v
